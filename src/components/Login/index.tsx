233e242b1be54219c883628c6a774e86e5f97d82
Jovane Maciel De Sousa <jovane.sousa@redeflex.com.br>
2025-11-14 15:51:00 -0400

    fix: melhorando leitura do código

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -6,35 +6,27 @@ import FormLogin from "../FormLogin";
 const Login = () => {
   const [isLoginPageActive, setIsLoginPageActive] = useState(true);
 
+  const title = isLoginPageActive ? "Login" : "Registrar";
+  const Form = isLoginPageActive ? FormLogin : FormRegister;
+  const buttonText = isLoginPageActive
+    ? "Criar uma conta"
+    : "Voltar para o login";
+
   return (
     <LoginSection>
       <div className="img">
         <img src="https://colorlib.com/etc/lf/Login_v1/images/img-01.png" />
       </div>
       <div className="form">
-        <span className="title">
-          {isLoginPageActive ? "Login" : "Registar"}
-        </span>
-        {isLoginPageActive ? (
-          <FormLogin />
-        ) : (
-          <FormRegister />
-        )}
-        {isLoginPageActive ? (
-          <button
-            className="create"
-          >
-            Criar uma conta{" "}
-            <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-          </button>
-        ) : (
-          <button
-            className="create"
-          >
-            Voltar para o login{" "}
-            <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-          </button>
-        )}
+        <span className="title">{title}</span>
+        <Form />
+        <button
+          onClick={() => setIsLoginPageActive(!isLoginPageActive)}
+          className="create"
+        >
+          {buttonText}
+          <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+        </button>
       </div>
     </LoginSection>
   );

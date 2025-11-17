237c4fae0d562a2f89e4d6839c152a980fabfb44
Jovane Maciel De Sousa <jovane.sousa@redeflex.com.br>
2025-11-17 14:43:33 -0400

    fix: ajustando validação formulario login para evitar erro 400 nas requisições

--- src/validations/loginSchema.ts
+++ src/validations/loginSchema.ts
@@ -9,4 +9,6 @@ export const loginSchema = yup.object({
   password: yup
     .string()
     .required("O campo Password é obrigatório")
+    .min(6, "O campo Password precisa ter no minimo 6 caracteres")
+    .max(100, "O campo Password precisa ter no máximo 100 caracteres")
 });

192910a2cb0749291419f79b0649617006202f40
Jovane Maciel De Sousa <jovane.sousa@redeflex.com.br>
2025-12-18 16:35:01 -0400

    feat: adicionando lazyload nas pages para minimizar o bundle

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,30 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import LoginPage from "../pages/LoginPage";
-import MainPage from "../pages/MainPage";
 import PrivateRoute from "./PrivateRoutes";
+import { lazy, Suspense } from "react";
+import Loader from "../components/Loader";
+
+const LoginPage = lazy(() => import("../pages/LoginPage"));
+const MainPage = lazy(() => import("../pages/MainPage"));
 
 const Rotas = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route
-        path="/*"
-        element={
-          <PrivateRoute>
-            <Routes>
-              <Route path="/home" element={<MainPage />} />
-            </Routes>
-          </PrivateRoute>
-        }
-      />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <Routes>
+                <Route path="/home" element={<MainPage />} />
+              </Routes>
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Suspense>
   );
 };
 

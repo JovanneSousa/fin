d228a48947c8a0b1e3e871c513dd61fdbeb79168
Jovane Maciel De Sousa <jovane.sousa@redeflex.com.br>
2026-02-11 17:43:10 -0400

    polindo a request wake

--- src/Hooks/useHealthApi.ts
+++ src/Hooks/useHealthApi.ts
@@ -3,14 +3,30 @@ import api from "../Services/api";
 
 const useHealthApi = () => {
   const apiEmail = import.meta.env.VITE_API_EMAIL_URL;
-  const wakeAuth = () => fetch(apiAuth + "api/auth/heath");
-  const wakeEmail = () => fetch(apiEmail + "/email/health");
-  const wakeTransacao = () => fetch(api + "api/transacoes/health");
+  const wakeAuth = () =>
+    fetch(apiAuth + "api/auth/heath", {
+      method: "GET",
+      mode: "no-cors",
+    });
+  const wakeEmail = () =>
+    fetch(apiEmail + "/email/health", {
+      method: "GET",
+      mode: "no-cors",
+    });
+  const wakeTransacao = () =>
+    fetch(api + "api/transacoes/health", {
+      method: "GET",
+      mode: "no-cors",
+    });
 
   const acordaApis = () => {
-    wakeAuth();
-    wakeEmail();
-    wakeTransacao();
+    try {
+      wakeAuth();
+      wakeEmail();
+      wakeTransacao();
+    } catch (e) {
+      console.log("Api acordando... " + e);
+    }
   };
 
   return acordaApis;

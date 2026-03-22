9435272978e45e4f8320d169ad478c3390094662
Jovanne Sousa <jovanemaciel943@gmail.com>
2026-03-22 18:07:20 -0400

    fix: corrigindo provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Provider, useSelector } from "react-redux";
-import { store, type RootReducer } from "./Store";
+import { useSelector } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Rotas from "./routes";
 import FormNewProvider from "./contexts/FormNew/FormNewProvider";
@@ -7,21 +6,20 @@ import "react-loading-skeleton/dist/skeleton.css";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./styles/theme";
 import { GlobalStyle } from "./styles/globalstyle";
+import { type RootReducer } from "./Store";
 
 function App() {
   const { dark } = useSelector((state: RootReducer) => state.theme);
 
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={dark ? darkTheme : lightTheme}>
-        <FormNewProvider>
-          <GlobalStyle />
-          <BrowserRouter>
-            <Rotas />
-          </BrowserRouter>
-        </FormNewProvider>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={dark ? darkTheme : lightTheme}>
+      <FormNewProvider>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Rotas />
+        </BrowserRouter>
+      </FormNewProvider>
+    </ThemeProvider>
   );
 }
 
